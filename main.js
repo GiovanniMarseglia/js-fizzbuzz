eb6a8a55a1adf475a2639bf65ffc3a15518791cc
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-01-09 17:07:30 +0100

    Completed

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+for(let i=1; i<101; i++){
+    
+    
+    if(i % 3){ 
+        if(i % 5){
+            document.getElementById("roww").innerHTML +=  (`<div class="col-2 g-5"><div class="bg-primary d-flex justify-content-center align-items-center" style="height:150px; width:150px;" > ${i}</div>
+                
+    </div>`)
+        }else{
+            document.getElementById("roww").innerHTML +=  (`<div class="col-2 g-5" ><div class="bg-warning d-flex justify-content-center align-items-center" style="height:150px; width:150px;" > Buzz </div>
+                
+    </div>`)
+        }
+    }else{ 
+
+        if(i % 5){
+            document.getElementById("roww").innerHTML +=  (`<div class="col-2 g-5" ><div class="bg-success d-flex justify-content-center align-items-center" style="height:150px; width:150px;" > Fizz </div>
+                
+    </div>`)
+        }else{
+            document.getElementById("roww").innerHTML +=  (`<div class="col-2 g-5" ><div class="bg-danger d-flex justify-content-center align-items-center" style="height:150px; width:150px;" > FizzBuzz </div>
+                
+    </div>`)
+        }
+
+
+
+    }
+
+
+    
+
+
+
+}
